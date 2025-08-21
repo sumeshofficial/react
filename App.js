baa75723cea55a87c598aa8b32812f28ef763b7a
sumeshofficial <sumeshsujesh1233@gmail.com>
2025-08-21 17:16:27 +0530

    ep01

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello I'am h1"),
+    React.createElement("h2", {}, "Hello I'am h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello I'am h1"),
+    React.createElement("h2", {}, "Hello I'am h2"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
